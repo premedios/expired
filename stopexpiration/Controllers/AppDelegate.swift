cadeb4517e5ff6d7426f09e080dcde7f6ec0e0da
premedios <pedro.remedios@gmail.com>
2015-06-11 20:54:52 +0100

    Initial Commit

---
+++ stopexpiration/Controllers/AppDelegate.swift
@@ -0,0 +1,14 @@
+//
+//  AppDelegate.swift
+//  stopexpiration
+//
+//  Created by Pedro Remedios on 6/11/15
+//  Copyright (c) 2015 Pedro Remedios. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+    var window: UIWindow?
+}
